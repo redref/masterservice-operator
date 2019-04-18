b8e1a4bcc893cde40e1f67ece6f41904fee3086d
Anthony Bescond <anthony.bescond@blablacar.com>
2019-04-18 09:45:31 +0200

    Add CRD + field ServiceTemplate

---
+++ Jenkinsfile
@@ -0,0 +1,2 @@
+gofmt -d pkg cmd
+operator-sdk generate k8s
---
+++ pkg/apis/blablacar/v1/doc.go
@@ -0,0 +1,4 @@
+// Package v1 contains API Schema definitions for the blablacar v1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=blablacar.com
+package v1
