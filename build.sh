231c4b0978750b35ce434885f126ccb1ffe824f8
Anthony Bescond <anthony.bescond@blablacar.com>
2019-04-19 16:56:22 +0200

    Helm chart + testing

--- build.sh
+++ build.sh
@@ -13,8 +13,7 @@ operator-sdk generate k8s
 operator-sdk build eu.gcr.io/infra-sandbox-58fe57e9/masterservice-operator:${VERSION}
 docker push eu.gcr.io/infra-sandbox-58fe57e9/masterservice-operator:${VERSION}
 
-kubectl apply -f deploy/crds/blablacar_v1_masterservice_crd.yaml
-kubectl apply -f deploy/service_account.yaml
-kubectl apply -f deploy/role.yaml
-kubectl apply -f deploy/role_binding.yaml
-sed -e 's|REPLACE_IMAGE|eu.gcr.io/infra-sandbox-58fe57e9/masterservice-operator:'"${VERSION}"'|g' deploy/operator.yaml | kubectl apply -f -
+helm install -n masterservice-operator ./helm
+echo "Waiting start"
+sleep 20
+helm test masterservice-operator --cleanup
